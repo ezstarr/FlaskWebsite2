7ee0780e11bc6116c391b77b50cdf0f18371a457
Esther <26940960+ezstarr@users.noreply.github.com>
2022-06-03 23:08:55 -0700

    styled result page

--- index.py
+++ index.py
@@ -4,11 +4,10 @@ import Fortune_deck as F_D
 import json
 import tarot
 
-
 app = Flask(__name__)
 
 # Introduces what I'm doin on the home page:
-title = "Hello! <br>This is the main page:" 
+title = "Hello! <br>This is the main page:"
 title += "Goal of today's stream: <br><h1>Basic Tarot Reader</h1>"
 title += "<a href=\"http://127.0.0.1:5000/submit\">tarot page</a>"
 
@@ -16,61 +15,44 @@ title += "<a href=\"http://127.0.0.1:5000/submit\">tarot page</a>"
 # The home page
 @app.route("/", methods=['GET', 'POST'])
 def home():
-	return render_template('index.html')
+    return render_template('index.html')
 
-# The reading result page.
-# @app.route("/result", methods=['GET', 'POST'])
-# def reading_result():
-# 	return render_template('reading_result.txt')
 
 @app.route("/admin")
 def admin():
-	return redirect(url_for("home"))
+    return redirect(url_for("home"))
+
 
 numofcards = 0
 
-# @app.route("/tarot.py", methods=['GET','POST'])
-# def tarot_reading():
-# 	tarot_card_names = tarot.get_tarot_names('tarot-images.json')
 
+@app.route('/result', methods=['GET', 'POST'])
+def creates_read_result_html_page():
+    name = request.args.get('name', 'N/A', type=str)
+    numofcards = request.args.get('numofcards', 1, type=int)
 
-	# print(tarot_deck)
+    if numofcards < 1:
+        numofcards = 1
+    if numofcards > 6:
+        numofcards = 6
 
-	# return render_template('tarot.py')
+    print(f"{numofcards}")
 
-# print(tarot_reading())
+    tarot_deck = F_D.TarotDeck()
+    sample = random.sample(tarot_deck.list_of_cards, numofcards)
 
+    # Show the descriptions for {numofcards} cards.
 
-@app.route('/submit', methods=['GET', 'POST'])
-def creates_index_html_page():
-	name = request.args.get('name', "N/A")
-	numofcards = request.args.get('numofcards', 1, type=int)
+    # return render_template('submit-tarot.html')
 
-	if numofcards < 1:
-		numofcards = 1
-	if numofcards > 6:
-		numofcards = 6
+    # r_variable = f"Hey {name}, you selected {numofcards}. \n"
 
-	print(f"{numofcards}")
+    # f"Your random cards are: \n\n {'<br>'.join(repr(samp) for samp in sample)}."
 
+    return render_template('reading_result.html', name=name, sample=sample)
 
-	tarot_deck = F_D.TarotDeck()
-	sample = random.sample(tarot_deck.list_of_cards, numofcards)
-
-
-
-	# Show the descriptions for {numofcards} cards.
-
-	return f"Hey {name}, you selected {numofcards}. Your random cards are: <br><br> {'<br>'.join(repr(samp) for samp in sample)}."
 
 
 
 if __name__ == "__main__":
-	app.run(debug=False)
-
-
-
-
-
-
-
+    app.run(debug=False)
